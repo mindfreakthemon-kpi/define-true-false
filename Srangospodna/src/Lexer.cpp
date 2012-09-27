ebd99f7373126fa318bde9bb57155d0cfba2595f
crashtua <crashtua@gmail.com>
2012-09-27 19:09:04 +0300

    added missing Lexer.cpp

---
+++ Srangospodna/src/Lexer.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   Lexer.cpp
+ * Author: CrashTUA
+ * 
+ * Created on 27 Сентябрь 2012 г., 18:54
+ */
+
+#include <stddef.h>
+
+#include "Lexer.h"
+
+Lexer::Lexer() {
+}
+
+Lexer::Lexer(const Lexer& orig) {
+}
+Token* Lexer::scan() {
+    return NULL;
+}
+Lexer::~Lexer() {
+}
+
